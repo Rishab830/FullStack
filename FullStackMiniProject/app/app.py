290e8ce51a56fb8513e0a66746c1811dd154c45e
Rishab <rishabrnair05@gmail.com>
2025-10-03 22:12:32 +0530

    meh

--- FullStackMiniProject/app/app.py
+++ FullStackMiniProject/app/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request
+from base64 import b64encode
+from bson import Binary
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client['BidHub']
 user_collection = db['Users']
+product_collection = db['Products']
+
+@app.context_processor
+def add_imports():
+    # Note: we only define the top-level module names!
+    return dict(io=io)
 
 @app.route('/')
 def home():
@@ -24,8 +34,9 @@ def auth():
     username = request.form.get('username')
     password = request.form.get('password')
     user = user_collection.find_one({"username": username, "password": password})
+    name = user['name']
     if user:
-        return render_template('buyer.html', name=user['name'])
+        return redirect(url_for('buyer', id=user['id'], name=name))
     else:
         return render_template('login.html', login_error='incorrect credentials')
     
@@ -39,17 +50,68 @@ def register():
     name = request.form.get('name')
     password = request.form.get('password')
     user_collection.insert_one({'id': id, 'name': name, 'password': password, 'email': email})
-    return render_template('buyer.html', name=user_collection.find_one({'id': id})['name'])
+    return redirect(url_for('buyer', id=user['id'], name=name))
 
 @app.route('/buyer', methods=['GET', 'POST'])
 def buyer():
-    name = request.args['name']
-    return render_template('buyer.html', name=name)
+    id = request.args['id']
+    name = user_collection.find_one({'id': int(id)})['name']
+    products = list(db.Products.find())
+    for product in products:
+        if product.get('image'):
+            product['image_base64'] = b64encode(product['image']).decode('utf-8')
+    return render_template('buyer.html', name=name, products=products, id=id)
 
 @app.route('/seller', methods=['GET','POST'])
 def seller():
-    name = request.args['name']
-    return render_template('seller.html', name=name)
+    id = request.args['id']
+    user = user_collection.find_one({'id': int(id)})
+    name = user['name']
+    return render_template('seller.html', id=id, name=name)
+
+@app.route('/listing', methods=['GET','POST'])
+def listing():
+    id = request.args['id']
+    user = user_collection.find_one({'id': int(id)})
+    name = user['name']
+    listings = list(product_collection.find({'user_id': int(id)}))
+    for listing in listings:
+        if listing.get('image'):
+            listing['image_base64'] = b64encode(listing['image']).decode('utf-8')
+        if int(listing['duration']) < 3:
+            listing['is_editable'] = False
+        else:
+            listing['is_editable'] = True
+        print(listing)
+    return render_template('listing.html', id=id, name=name, listings=listings)
+
+@app.route('/product', methods=['GET', 'POST'])
+def product():
+    user_id = int(request.args['user_id'])
+    name = user_collection.find_one({'id': int(user_id)})['name']
+    if request.method == 'POST':
+        product_id = product_collection.count_documents({}) + 1
+        title = request.form['title']
+        description = request.form['description']
+        image_file = request.files['image']
+        price = int(request.form['startingPrice'])
+        duration = request.form['duration']
+        category = request.form['category']
+        condition = request.form['condition']
+        if image_file:
+            image_data = image_file.read()
+            product_collection.insert_one({
+                'user_id': user_id,
+                'product_id': product_id,
+                'title': title,
+                'description': description,
+                'price': price,
+                'duration': duration,
+                'category': category,
+                'condition': condition,
+                'image': Binary(image_data)
+            })
+        return redirect(url_for('buyer', id=user_id, name=name))
 
 if __name__ == '__main__':
     app.run(debug=True)
